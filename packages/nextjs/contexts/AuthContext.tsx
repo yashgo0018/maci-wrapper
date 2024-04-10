c945ae81f32c85b31dc964fdff7c18fba60922f8
Yash Goyal <yashgo0018@gmail.com>
2024-04-10 15:46:48 +0530

    added state index of the user key to auth context

--- packages/nextjs/contexts/AuthContext.tsx
+++ packages/nextjs/contexts/AuthContext.tsx
@@ -3,12 +3,15 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { Keypair } from "@se-2/hardhat/maci-ts/domainobjs";
 import { useAccount } from "wagmi";
-import { useScaffoldContractRead, useScaffoldEventSubscriber } from "~~/hooks/scaffold-eth";
+import deployedContracts from "~~/contracts/deployedContracts";
+import { useScaffoldContractRead, useScaffoldEventHistory, useScaffoldEventSubscriber } from "~~/hooks/scaffold-eth";
+import scaffoldConfig from "~~/scaffold.config";
 import { fetchOrCreateUserKeyPair } from "~~/utils/crypto";
 
 interface IAuthContext {
   isRegistered: boolean;
   keypair: Keypair | null;
+  stateIndex: bigint | null;
 }
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
@@ -16,6 +19,7 @@ export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 export default function AuthContextProvider({ children }: { children: React.ReactNode }) {
   const { address } = useAccount();
   const [keypair, setKeyPair] = useState<Keypair | null>(null);
+  const [stateIndex, setStateIndex] = useState<bigint | null>(null);
 
   useEffect(() => {
     setKeyPair(fetchOrCreateUserKeyPair(address));
@@ -27,20 +31,52 @@ export default function AuthContextProvider({ children }: { children: React.Reac
     args: keypair ? keypair.pubKey.rawPubKey : [undefined, undefined],
   });
 
+  const chainId = scaffoldConfig.targetNetworks[0].id;
+
+  const {
+    MACI: { deploymentBlockNumber },
+  } = deployedContracts[chainId];
+
+  const { data: SignUpEvents } = useScaffoldEventHistory({
+    contractName: "MACI",
+    eventName: "SignUp",
+    filters: {
+      _userPubKeyX: keypair?.pubKey.asContractParam().x,
+      _userPubKeyY: keypair?.pubKey.asContractParam().y,
+    },
+    fromBlock: BigInt(deploymentBlockNumber),
+  });
+
+  useEffect(() => {
+    if (!keypair || !SignUpEvents || !SignUpEvents.length) {
+      setStateIndex(null);
+      return;
+    }
+
+    const lastSignUpEvent = SignUpEvents[SignUpEvents.length - 1];
+    setStateIndex(lastSignUpEvent.args._stateIndex || null);
+  }, [keypair, SignUpEvents]);
+
   useScaffoldEventSubscriber({
     contractName: "MACI",
     eventName: "SignUp",
     listener: logs => {
       logs.forEach(log => {
-        log.args._userPubKeyX === keypair?.pubKey.asContractParam().x &&
-          log.args._userPubKeyY === keypair?.pubKey.asContractParam().y &&
-          refetchIsRegistered();
+        if (
+          log.args._userPubKeyX !== keypair?.pubKey.asContractParam().x ||
+          log.args._userPubKeyY !== keypair?.pubKey.asContractParam().y
+        )
+          return;
+        refetchIsRegistered();
+        setStateIndex(log.args._stateIndex || null);
       });
     },
   });
 
   return (
-    <AuthContext.Provider value={{ isRegistered: Boolean(isRegistered), keypair }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ isRegistered: Boolean(isRegistered), keypair, stateIndex }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
